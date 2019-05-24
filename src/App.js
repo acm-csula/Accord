33e8742304719b8fe5c6a01cc14024699ec933f2
deric <derickwok25@gmail.com>
2019-05-23 19:09:49 -0700

    finished login

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './images/accordlogoname.png';
 import './css/App.css';
+import 'bootstrap/dist/css/bootstrap.css';
 
 function App() {
   return (
-    <div className="login-container justify-content-center">
+    <div className="login-container">
+      <img className="login-logo" src={logo}></img>
       <div className="login-btn-container">
-        <a className="login-txt"> Login </a> 
+        <a className="login-txt"> Login </a> <br></br>
+        <a className="text-muted small-text"> with Google.</a>
       </div>
     </div>
   );
