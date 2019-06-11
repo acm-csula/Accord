7d04563b0fd86608a60ed470b3dda297bf99a1e1
deric <derickwok25@gmail.com>
2019-06-10 19:06:00 -0700

    working dropdown
    made it with pure html/css
    dankmemememeemememms

--- src/components/home.js
+++ src/components/home.js
@@ -34,7 +34,7 @@ logout() {
               <div className="col">
                   <div className="row">
                       <div className="col-md-2 left-container">
-                          <div className="server-title-header user-setting-btn">
+                          <div className="server-title-header">
                               <a className="server-title"> ACM @CSULA
                                 <i class="fas fa-sort-down"></i>
                               </a>
@@ -42,35 +42,22 @@ logout() {
                                 <i class="fas fa-circle"></i>
                                 <a> morgan freeman </a>
                               </div>
+                              <div className="user-menu">
+                                <button className="user-stats"> <i class="fas fa-circle"></i> Set a Status.. </button>
+                                <button className="account-set"> <i class="fas fa-user"></i>Profile & Account </button>
+                                <div className="divider"></div>
+                                <button className="server-set"> <i class="fas fa-cog"></i>Server Settings</button>
+                                <button className="create-chan"><i class="fas fa-plus"></i>Create Channel</button>
+                                <button className="notifi"><i class="far fa-bell"></i>Notifications Settings</button>
+                                <div className="divider"></div>
+                                <button className="logout-btn" onClick = {this.logout}><i class="fas fa-sign-out-alt"></i>Logout</button>
+                              </div>
                           </div>
                           
                       </div>
                       <div className="col-md-8 right-container"> ur m0m gey </div>
                   </div>
               </div>
-          </div>
-          <div className="user-menu">
-            <button className="user-stats"> <i class="fas fa-circle"></i>
-            Set a Status.. </button>
-            <button className="account-set"> <i class="fas fa-user"></i>
-            Profile & Account 
-            </button>
-            <div className="divider"></div>
-            <button className="server-set"> <i class="fas fa-cog"></i>
-            Server Settings 
-            </button>
-            <button className="create-chan"> 
-            <i class="fas fa-plus"></i>
-            Create Channel 
-            </button>
-            <button className="notifi"> 
-            <i class="far fa-bell"></i>
-            Notifications Settings
-            </button>
-            <div className="divider"></div>
-            <button className="logout-btn" onClick = {this.logout}>
-                <i class="fas fa-sign-out-alt"></i>
-                Logout</button>
           </div>
         </div>
 
